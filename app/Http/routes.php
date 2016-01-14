6a40902b6d3edcd3b6f57fa28b046222dcd53fb9
Pedro Nogueira <pvcnogueira@outlook.com>
2016-01-14 14:08:30 -0200

    Criando os primeiros Models

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,3 +13,6 @@
 
 Route::get('/', 'WelcomeController@index');
 Route::get('exemplo', 'WelcomeController@exemplo');
+
+Route::get('admin/categories', 'AdminCategoriesController@index');
+Route::get('admin/products', 'AdminProductsController@index');
